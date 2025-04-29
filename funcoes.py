a97349dff3dfd90fbc97fb68169389c823ff53bc
Henrique <henriquesa3@al.insper.edu.br>
2025-04-29 16:36:56 -0300

    exs 7

--- funcoes.py
+++ funcoes.py
@@ -60,3 +60,11 @@ def calcula_pontos_sequencia_baixa (lista):
         return 15
     else:
         return 0
+
+def calcula_pontos_sequencia_alta (lista):
+    if 1 in lista and 2 in lista and 3 in lista and 4 in lista and 5 in lista:
+        return 30
+    elif 2 in lista and 3 in lista and 4 in lista and 5 in lista and 6 in lista:
+        return 30
+    else:
+        return 0
